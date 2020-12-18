6270675169271b5f1216e53140c77458dfc9ea89
landwarrior <tech_land_equilibrium@outlook.jp>
2020-12-18 11:56:06 +0900

    add sample code

--- src/js/my.js
+++ src/js/my.js
@@ -11,7 +11,7 @@ $(function() {
   $('#cont1_1').blur(function() {
     let result1_1 = $('#result1_1').text();
     $('#result1_1').text(result1_1 + 'blurされた');
-  })
+  });
 
   // changeイベントを拾った時の動作
   $('#cont2_1').change(function() {
@@ -36,23 +36,42 @@ window.onload = function() {
   document.getElementById('cont1_2').onfocus = function() {
     let result1_2 = document.getElementById('result1_2').innerText;
     document.getElementById('result1_2').innerText = result1_2 + 'focusされた';
-  }
+  };
 
   document.getElementById('cont1_2').onblur = function() {
     let result1_2 = document.getElementById('result1_2').innerText;
     document.getElementById('result1_2').innerText = result1_2 + 'blurされた';
-  }
+  };
 
   document.getElementById('cont2_2').onchange = function() {
     let cont2_2 = document.getElementById('cont2_2').value;
     document.getElementById('result2_2').innerText = cont2_2;
-  }
+  };
 
   document.getElementById('cont3_2').oninput = function() {
     let cont3_2 = document.getElementById('cont3_2').value;
     document.getElementById('result3_2').innerText = cont3_2;
+  };
+
+  // ループ処理の試し
+  let start = Date.now();
+  for (let i = 0; i < 10000; i++) {
+    const elements = document.querySelectorAll('#table1 tbody tr');
+    for (const elem of elements) {
+      let test = elem.querySelector('td:nth-child(3)').innerText;
+    }
   }
-}
+  let end = Date.now();
+  document.getElementById('result_native').innerText = end - start;
+  start = Date.now();
+  for (let i = 0; i < 10000; i++) {
+    $('#table1 tbody tr').each(function() {
+      let test = $(this).find('td:nth-child(3)').text();
+    });
+  }
+  end = Date.now();
+  document.getElementById('result_jquery').innerText = end - start;
+};
 
 // 先頭に書いたjQueryのサンプルは、以下のように書いてもいい
 $(document).ready(function() {
